4c3b91469f32786b774c121e5e2cdd99dddf6fbc
p00n00b <52492927+p00n00b@users.noreply.github.com>
2022-07-15 19:48:02 +0300

    Update sub_enum.sh

--- sub_enum.sh
+++ sub_enum.sh
@@ -5,3 +5,5 @@ cat tmp.txt | dnsgen - >> tmp.txt
 massdns -r dns-resolvers.txt -t $2 -o S -w  out.txt tmp.txt
 cat out.txt | sort -u > $1_domains.txt
 subs.sh $1 bin/$1_domains.txt | httpx  -status-code -location -ip -o $1_httpx.txt
+rm /root/go/bin/out.txt
+rm /root/go/bin/tmp.txt
